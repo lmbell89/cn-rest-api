ff5165af607934d85b4464b2617f28b995d3db4e
Liam Bell <l.m.bell89@gmail.com>
2021-03-31 11:00:07 +0100

    updated user get and post routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 require('./db/connection')
 const express = require('express')
+
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.json())
-
-const port = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -19,19 +19,20 @@ app.get('/user', async function (req, res) {
     const allUsers = await User.find({})
     res.send(allUsers)
   } catch (error) {
-    res.send(error)
+    res.status(500).send(error)
   }
 })
 
 app.post('/user', async function (req, res) {
   try {
-    const allUsers = await User.find({})
-    res.send(allUsers)
+    const user = new User(req.body)
+    const returnedValue = await user.save()
+    res.status(201).send(returnedValue)
   } catch (error) {
-    res.send(error)
+    res.status(400).send(error)
   }
 })
  
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
